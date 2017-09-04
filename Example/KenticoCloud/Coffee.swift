b138c3cc37e6980b225198b34cf3b55316454f47
Martin Makarsky <martinmakarsky@gmail.com>
2017-09-04 16:28:51 +0200

    Strong type mapping for NumberElement

--- Example/KenticoCloud/Coffee.swift
+++ Example/KenticoCloud/Coffee.swift
@@ -18,14 +18,27 @@ public class Coffee: Mappable {
 //    var shortDescription: String?
 //    var longDescription: String?
     var callToActions: ModularContentElement?
+    var farm: TextElement?
+    var country: TextElement?
+    var variety: TextElement?
+    var processing: MultipleChoiceElement?
+    var altitude: TextElement?
+    var price: NumberElement?
 
     public required init?(map: Map){
         let mapper = MapElement.init(map: map)
+        
         name = mapper.map(elementName: "coffee_name", elementType: TextElement.self)
         photo = mapper.map(elementName: "photo", elementType: AssetElement.self)
         category = mapper.map(elementName: "coffee_category", elementType: TaxonomyElement.self)
         promotion = mapper.map(elementName: "promotion", elementType: MultipleChoiceElement.self)
         callToActions = mapper.map(elementName: "call_to_actions", elementType: ModularContentElement.self)
+        farm = mapper.map(elementName: "farm", elementType: TextElement.self)
+        country = mapper.map(elementName: "country", elementType: TextElement.self)
+        variety = mapper.map(elementName: "variety", elementType: TextElement.self)
+        processing = mapper.map(elementName: "processing", elementType: MultipleChoiceElement.self)
+        altitude = mapper.map(elementName: "altitude", elementType: TextElement.self)
+        price = mapper.map(elementName: "price", elementType: NumberElement.self)
     }
     
     public func mapping(map: Map) {
