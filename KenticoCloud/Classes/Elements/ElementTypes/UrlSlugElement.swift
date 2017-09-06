e20ce8ec9e3805eac4b58f2f5898e08cb5074581
Martin Makarsky <martinmakarsky@gmail.com>
2017-09-06 14:03:10 +0200

    Strong type mapping for UrlSlugElement

---
+++ KenticoCloud/Classes/Elements/ElementTypes/UrlSlugElement.swift
@@ -0,0 +1,30 @@
+//
+//  UrlSlugElement.swift
+//  Pods
+//
+//  Created by Martin Makarsky on 06/09/2017.
+//
+//
+
+import ObjectMapper
+
+public class UrlSlugElement: Mappable {
+    public private(set) var type: String?
+    public private(set) var name: String?
+    public private(set) var value: String?
+    
+    public required init?(map: Map){
+    }
+    
+    public func mapping(map: Map) {
+        var elementName = ""
+        if let context = map.context as? ElementContext {
+            elementName = context.elementName
+        }
+        
+        type <- map["elements.\(elementName).type"]
+        name <- map["elements.\(elementName).name"]
+        value <- map["elements.\(elementName).value"]
+    }
+}
+
