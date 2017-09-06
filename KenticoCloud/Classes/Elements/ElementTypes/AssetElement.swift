a86661b96a65fad66eae0a586dd0188f15988960
Martin Makarsky <martinmakarsky@gmail.com>
2017-09-06 13:23:17 +0200

    Consolidate Xcode and file system file structure part 2

---
+++ KenticoCloud/Classes/Elements/ElementTypes/AssetElement.swift
@@ -0,0 +1,29 @@
+//
+//  AssetElement.swift
+//  Pods
+//
+//  Created by Martin Makarsky on 04/09/2017.
+//
+//
+
+import ObjectMapper
+
+public class AssetElement: Mappable {
+    public private(set) var type: String?
+    public private(set) var name: String?
+    public private(set) var value: [Asset]?
+    
+    public required init?(map: Map){
+    }
+    
+    public func mapping(map: Map) {
+        var elementName = ""
+        if let context = map.context as? Context {
+            elementName = context.elementName
+        }
+        
+        type <- map["elements.\(elementName).type"]
+        name <- map["elements.\(elementName).name"]
+        value <- map["elements.\(elementName).value"]
+    }
+}
