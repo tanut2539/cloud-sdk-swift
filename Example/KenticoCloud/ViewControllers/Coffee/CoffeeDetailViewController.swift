bf94d992f4f1a1b2f53a835ec71c890aeefec844
Martin Makarsky <martinmakarsky@gmail.com>
2017-09-20 10:20:35 +0200

    Draft for Coffee detail view. Removed SettingsViewController

--- Example/KenticoCloud/ViewControllers/Coffee/CoffeeDetailViewController.swift
+++ Example/KenticoCloud/ViewControllers/Coffee/CoffeeDetailViewController.swift
@@ -9,15 +9,45 @@
 import UIKit
 
 class CoffeeDetailViewController: UIViewController {
-
-    var coffee: Coffee?
+    @IBOutlet var coffeeDescription: UIWebView!
+    @IBOutlet var price: UILabel!
+    @IBOutlet var farm: UILabel!
+    @IBOutlet var variety: UILabel!
+    @IBOutlet var processing: UILabel!
+    @IBOutlet var altitude: UILabel!
+    @IBOutlet var coffeeImage: UIImageView!
+    
+    var coffee: Coffee!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.title = coffee.name?.value
+        if let description = coffee.longDescription?.value {
+            coffeeDescription.loadHTMLString(description , baseURL: nil)
+        }
+        if let price = coffee.price?.value {
+            self.price.text = "$\(price) / 1lb"
+        }
+        self.farm.text = coffee.farm?.value
+        self.variety.text = coffee.variety?.value
+        if !((coffee.processing?.value?.isEmpty)!) {
+            if let processingTechnique = coffee.processing?.value?[0].name {
+                self.processing.text = processingTechnique
+            }
+        }
+        self.altitude.text = coffee.altitude?.value
+        
+        if let imageUrl = coffee.photo?.value?[0].url {
+            let url = URL(string: imageUrl)
+            coffeeImage.af_setImage(withURL: url!)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
