d602b54823c07f12714c3d20bc1aeb7be054f739
Martin Makarsky <martinmakarsky@gmail.com>
2017-09-21 11:16:17 +0200

    Tests for GetItems

---
+++ Example/Tests/TestConstants.swift
@@ -0,0 +1,11 @@
+//
+//  TestConstants.swift
+//  KenticoCloud
+//
+//  Created by Martin Makarsky on 21/09/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+struct TestConstants {
+    static let projectId = "adcae48f-b42b-4a53-a8fc-b3b4501561b9"
+}
