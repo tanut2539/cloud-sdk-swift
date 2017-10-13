46facb4f58d43fac540b36011d035cbef2da2d76
Martin Makarsky <martinmakarsky@gmail.com>
2017-10-13 14:15:00 +0200

    TDD - tests for content types

--- Example/Tests/DeliveryService/DeliveryClient/GetContentTypes.swift
+++ Example/Tests/DeliveryService/DeliveryClient/GetContentTypes.swift
@@ -18,18 +18,18 @@ class GetContentTypesSpec: QuickSpec {
             
             //MARK: Get all content types
             
-            context("getting all content types", {
+            context("getting some content types", {
                 
-                it("returns list of all content types") {
+                it("returns list of some content types") {
                     
                     waitUntil(timeout: 5) { done in
-                        client.GetContentTypes(completionHandler: { (isSuccess, contentTypesResponse, error) in
+                        client.getContentTypes(skip: 2, limit: 4, completionHandler: { (isSuccess, contentTypesResponse, error) in
                             if !isSuccess {
                                 fail("Response is not successful. Error: \(String(describing: error))")
                             }
                             
                             if let response = contentTypesResponse {
-                                expectedCount = 13
+                                expectedCount = 4
                                 expect(response.contentTypes.count) == expectedCount
                                 done()
                             }
