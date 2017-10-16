79d5e105b4304bd67e8573873878680a8591d57c
Martin Makarsky <martinmakarsky@gmail.com>
2017-10-16 11:15:52 +0200

    Add support for elements in content types

--- KenticoCloud/Classes/DeliveryService/Models/ContentType.swift
+++ KenticoCloud/Classes/DeliveryService/Models/ContentType.swift
@@ -8,13 +8,15 @@
 
 import ObjectMapper
 
-/// Represents Content Type.
+ç
 public class ContentType: Mappable {
     
     /// Name of the content type.
     public private(set) var name: String?
     /// Codename of the content type.
     public private(set) var codename: String?
+    /// Contentype's elements.
+    public private(set) var elements: Dictionary<String, Element>?
     
     /// Maps response's json instance of the content type into strongly typed object representation.
     public required init?(map: Map) {
@@ -24,5 +26,6 @@ public class ContentType: Mappable {
     public func mapping(map: Map) {
         name <- map["system.name"]
         codename <- map["system.codename"]
+        elements <- map["elements"]
     }
 }
